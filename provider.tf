abf7c06fd84322eabba8ea442a874fd7a09f8f99
patilajayv <90689474+patilajayv@users.noreply.github.com>
2021-10-31 15:04:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,8 +9,8 @@ terraform {
 }
 provider "azurerm" {
   features { }
-  client_secret = "Dga7Q~Vl0hdGxEOqjOpqbw6WEALk9qg~7f2DZ"
-  client_id = "39887aa8-c8af-4d0c-9345-4d7235c78881"
+  client_secret = "1BI7Q~8nkUxOlnvG0lXergTM.4M5h4QSDJkVH"
+  client_id = "0a123ec8-46a3-4a31-9e10-1ad502657015"
   tenant_id = "eeeb6497-738c-49c7-b0cd-68cd0669e13d"
   subscription_id = "a680f040-a5db-4a85-b40f-61bf75c5aa4c"
 }
